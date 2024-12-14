d87c4c3ad39e37a0f09302d2c6b0a78b44be3dff
Anna Steckel <annaextrasteckel@gmail.com>
2024-12-13 22:45:04 -0800

    starting to play around with styling

--- README.md
+++ README.md
@@ -1,60 +1,53 @@
-# GAME NAME/APPLICATION TITLE
+# OH, FLOCK!
 
-#### INSERT 1 SENTANCE SUMMARY OF YOUR APPLICATION/GAME HERE
+#### Locate each bird duo before the turns run out!
 <img src="path to your game screenshot" alt="Description of Screenshot"/>
 
 <img src="path to your gif/video" style="width:100vw; height:100vh" alt="Text describing your video"/>
 
 ## Description
-Description of your game. Ex: Connect Four: a two player connection game. First player to get four of the same colored checkers in a row either horizontally, vertically or diagonally wins.
+A Wingspan-themed, single player game memory game where the player flips two cards over per turn. If the cards are a match, they remain upright and the user is one step closer to winning. If the cards don't match, they both flip back over again. The user has ten turns total to find all matches before the game is over. Hope your memory serves you well! 
+
+## Quick Links
+* **Project planning** can be found [here](https://trello.com/b/XxLPtSdn/memory-game).
+* **Wireframe** can be found [here](https://lucid.app/lucidchart/8aaecf84-a967-4391-a5d9-028f45b1acea/edit?beaconFlowId=BC1A14C6C4D919B2&invitationId=inv_2524a5b4-53ef-4e3e-a3c1-3fe477d4d70a&page=0_0#).
+* **GitHub repo** can be found [here](https://github.com/annamiriams/memory-game).
+* **Deployed project** link can be found [here]().
 
 ## Table of Contents
 * [Technologies Used](#technologiesused)
 * [Features](#features)
 * [Design](#design)
 * [Project Next Steps](#nextsteps)
-* [Deployed App](#deployment)
 * [About the Author](#author)
+* [Works Cited](#workscited)
 
 ## <a name="technologiesused"></a>Technologies Used
 * JavaScript
 * HTML5
 * CSS3
 
-
-## Features
-Users Feature 1
-Users Feature 2
-
-## Wireframe Images
-* Description of image location, or <img src="path to wireframe images">
-
-## Trello Planning
-* Link to trello or planning board - ex: https://trello.com/b/2jeHdFcV/project-1-connect-4
+## <a name="features"></a>Features
+* Users can play a memory card game where the goal is to match pairs of hidden cards.
+* Users can...
 
 ## <a name="design"></a>Design
-* Descrition of what inspired your design. Ex: Design elements implemented using HTML5 and CSS3. 
+* This game utilizes illustrations from the board game Wingspan. As a birdwatcher and board game enthusiast, Wingspan is one of my all time favorites. A memory game of this size is not typically considered a particularly challenging web browser game, so I attempted to make it a tad trickier by including birds with similar colored plumage. 
 
+Descrition of what inspired your design. Ex: Design elements implemented using HTML5 and CSS3. 
 
 ## <a name="nextsteps"></a>Project Next Steps
-#### List of Future Features
-* Allow the user to chose player one or player two.
-* Allow users to play against computer.
-* Add a timer
-* Add a scoreboard. 
-* Users can change the theme.
-
-## <a name="deployment"></a>Deployed Link
-[Netlify]([https://wonderful-brahmagupta-6a75d3.netlify.com](https://connect4pc.netlify.app/))
-
-* You can view the repository:
-[Github.com](https://github.com/Gr8ness21/Connect-4)
-* If unable to view please go live locally through VS Code
+* Add more card pairs on deck, allowing the user to choose a custom-sized board (i.e. 10 pairs, 14 pairs, or 18 pairs possible).
+* Replace the tally board with a timer.
+* Add visual effects to pair with a matched pair, win, and/or loss.
+* 
 
 ## <a name="author"></a>About The Author
+I'm new to coding, but experienced at problem-solving. 
+
 I build applications and mini projects tied to my various interests. I look for creative solutions to real world problems and think of technical ways to address them. While no application is ever perfect I find joy in the process and all my final products!
-    
-## Works Cited:
+
+## <a name="workscited"></a>Works Cited:
 *https://stackoverflow.com/questions/24093290/what-does-the-question-mark-mean-in-javascript/24093304
 *https://www.w3schools.com/jquery/jquery_ref_events.asp
 *Understanding “this” in JQUERY: 
--- js/app.js
+++ js/app.js
@@ -1,144 +1,80 @@
-/*
-As a player, I want to see a landing page when I arrive on the game’s website to know that I’m ready to play the game.
-As a player, I want to clearly see the instructions for the game presented, and then be able to click “x” to minimize those instructions. 
-As a player, I want to click on a “help” icon so that I can reread the instructions as needed throughout the game.
-As a player, I want to click on a “play” button so that I can start the game.
-As a player, I want to click on a card so that it flips over and reveals what is on the other side. 
-As a player, I want to see that two cards can remain flipped over at once, and then turn back over if they don’t match.
-As a player, I want to see visual feedback on the page when I have correctly selected two cards that match, and then to see that the two matched cards remain face up as I proceed with the game.
-As a player, I want to see the “turns” icon iterate so that I can track how many turns I have made and how many turns I have remaining. 
-As a player, I want to see the “matches” icon iterate so that I can track how many card pairs I have correctly selected.
-As a player, I want to see visual feedback on the page when I have correctly selected all matching cards within the allotted time or total number of turns (thereby winning the game).
-As a player, I want to see visual feedback on the page when I have not correctly selected all matching cards within the allotted time or total number of turns (thereby losing the game).
-As a player, whether I win or lose, I want to be able to click a “play again” button to restart the game.
-As a player, I want to click a “reset” button in the middle of the game so that I can reset the game to starting state. 
-*/
-
 /*-------------------------------- Constants --------------------------------*/
 
-// create constant to hold the total number of turns available to the player for each game
 const turnsAvailable = 10;
-// create constant to hold the total number of possible matches
 const matchesAvailable = 6;
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-// create variable that represents the number of turns the player plays, and that will iterate each turn (turn = 2 cards flipped)
 let turnsTally = 0;
-// create variable that represents the number of correct matches the player has identified, and that will iterate with each correct match made
 let matchesTally = 0;
-// create variable that represents whether the card flipped or not
 let cardFlipped = false;
-// create two variables to hold values for two flipped cards
 let firstCard;
 let secondCard;
-// create a variable that represents whether the board is frozen or not (in other words: can more cards be clicked right now? y/n)
-let freezeBoard = false;
+let boardFrozen = false;
 
 /*------------------------ Cached Element References ------------------------*/
 
-// create a cached element that will select all card elements
 const cards = document.querySelectorAll('.card');
-// used to print a message when a correct match is selected
-const matchFoundMsg = document.querySelector('#match-success');
-// used to iterate each correct match
-const matchesTallyMsg = document.querySelector('#matches-tally');
-// used to iterated each turn taken
-const turnsTallyMsg = document.querySelector('#turns-tally');
-// create a cached element that used to indicate if the player has lost or won
+const matchSuccessMsg = document.querySelector('#match-success');
+const matchesTallyIcon = document.querySelector('#matches-tally');
+const turnsTallyIcon = document.querySelector('#turns-tally');
 const resultsMsg = document.querySelector('#results');
 
 /*-------------------------------- Functions --------------------------------*/
 
-// create function that allows player to click on each cardBack, resulting in it flipping to cardFront
-const flipCard = function() {    
-    // issue: i can click many cards in succession and checkForMatch seems to get all screwy. it seems like creating the variable freezeBoard will help: at round start, freezeBoard is false (therefore board is not frozen). we need to freeze the board after the second click to allow time for checkForMatch to run, and then once checkForMatch is complete (which really means when flipCardsBack is complete since that's at the end of checkForMatch), we can set freezeBoard back to false.
-
-    // we have to lock the board until the incorrect card match is done flipping back to starting position to be able to click on more cards
-    // return used here means that when flipCard is run, and if freezeBoard === true, the rest of the function will not run. 
-    if (freezeBoard === true) {
-        return;
-    }
-    //ultimately what this line is doing is saying that for each time this function is called (each time a card is clicked), "this" card adds a class called 'flip'(which is written in our css) 
-    // i originally used .toggle (to toggle between two classes) below, but by the time i ended up adding in the code .removeEventListener, i think it might make more sense to pair that with .add (add & remove fits together better than toggle & remove). so switching .toggle to .add and i seem to get the same end results.
-    this.classList.add('flip');
-    // create if statement: if cardFlipped is false (meaning card has not flipped, set it to true instead, and set firstCard = to this state (in other words: all of what just happened is going to be assigned to firstCard))
-    if (cardFlipped === false) {
-        cardFlipped = true;
-        // same kind of use of 'this' as earlier: firstCard is assigned to 'this' most recent action(s)
-        firstCard = this;
-    }
-    // assign card click to secondCard, and change cardFlipped back to false 
-    else {        
-        secondCard = this;
-        cardFlipped = false;
-    // call the function written to check whether firstCard and secondCard match, then incrememt tallies, then compare() to update the message as needed 
-        checkForMatch();
-        incTurnsTally();
-        compare();
+const flipCard = function () {
+    if (boardFrozen === false) {
+        this.classList.add('flip');
+        if (cardFlipped === false) {
+            cardFlipped = true;
+            firstCard = this;
+        }
+        else {
+            secondCard = this;
+            cardFlipped = false;
+            checkForMatch();
+            incTurnsTally();
+            renderResults();
+        };
     };
 };
 
-// create a function that checks if the ids of firstCard matches secondCard. if they match, the cards should remain upright. if they don't match, the cards should flip back over.
-const checkForMatch = function() {
+const checkForMatch = function () {
     if (firstCard.id === secondCard.id) {
+        matchSuccessMsg.textContent = `You found two ${firstCard.id}s!`
         freezeCards();
-        // firstCard.id doesn't seem like the best way to write that, but ultimately i want a message to display indicating that a correct match was made and i can't currently think of a more sensible way of doing it
-        matchFoundMsg.textContent = `You found two ${firstCard.id}s!`
         incMatchesTally();
-        compare();
     }
     else {
         flipCardsBack();
-    }
+    };
 };
 
-// create a function that is called when the ids of firstCard and secondCard match (within checkForMatch). maybe this is where i'll later add some kind of visual component to run too?
-const freezeCards = function() {
-    // remove eventListeners so the cards can't be flipped again (meaning they can't be turned back over again)
+const freezeCards = function () {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 };
 
-// create a function that is called when the ids of the firstCard and secondCard DO NOT match. the function should flip the two cards back over. the setTimeout() method should work here by running a function after a certain amount of time has elapsed ('1000').
-const flipCardsBack = function() {
-    // first freeze the board if the cards do not match. 
-    freezeBoard = true;
+const flipCardsBack = function () {
+    boardFrozen = true;
     setTimeout(() => {
-        // remove the class 'flip' so that the cards flip back over (by triggering the 180deg turn)
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
-        // then unfreeze the board after the cards have been flipped back to starting state, which allows two more cards to be clicked
-        freezeBoard = false;
-    }, '1000'); // 1000 = 1 sec
+        boardFrozen = false;
+    }, '1000');
 };
 
-// create a function that increments the turns tally after any two cards are clicked and shows the results on the corresponding section
 const incTurnsTally = function () {
     turnsTally++;
-    turnsTallyMsg.textContent = `${turnsTally} / ${turnsAvailable} turns`
+    turnsTallyIcon.textContent = `${turnsTally} / ${turnsAvailable} turns`
 };
 
-//create a function that increments the matches tally after a correct match is found and shows the results on the corresponding section
 const incMatchesTally = function () {
     matchesTally++;
-    matchesTallyMsg.textContent = `${matchesTally} / ${matchesAvailable} matches`
+    matchesTallyIcon.textContent = `${matchesTally} / ${matchesAvailable} matches`
 };
 
-// create a function to shuffle the cards. since i used flexbox, i'm trying to  assign the cardPosition to the flex order property. i used the rock paper scissors exercise where we used the Math method to randomize the cardPosition from 12 positions total. then assigned that to the card order. 
-const shuffleCards = function() {
-    cards.forEach((card)=>{
-        const cardPosition = Math.floor(Math.random() * 12);
-        card.style.order = cardPosition
-    });
-};
-// i'm leaving this here because i haven't quite figured out if there's a better place for it to go. i just know it needs to run. maybe at the start of the game? maybe at the start and end of each round? maybe called when "reset" is clicked? right now the cards shuffle whenever the page is refreshed.
-shuffleCards();
-
-// create function to identify game end when one of these two results has been met:
-    // - the player has correctly matched all card pairs within the allotted number of turns (win): msg = "You won! Play again?"
-    // - the player has met the total turns allowed without correctly matching all card pairs (lose): msg = "You lost! Try again?"
-const compare = function() {
+const renderResults = function () {
     if (matchesTally === matchesAvailable) {
         resultsMsg.textContent = 'You won! Play again?';
     } else if (turnsTally === turnsAvailable) {
@@ -148,22 +84,20 @@ const compare = function() {
     };
 };
 
+const shuffleCards = function () {
+    cards.forEach((card) => {
+        const cardPosition = Math.floor(Math.random() * 12);
+        card.style.order = cardPosition
+    });
+};
+
+shuffleCards();
+
 /*----------------------------- Event Listeners -----------------------------*/
 
-// create an event listener for each click, specifically on each card
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
+    card.addEventListener('mouseover', function() {
+        this.style.cursor = 'pointer';
+    });
 });
-
-
-
-
-
-
-// extra stuff for later:
-
-// const: create a cached element that will select the id reset-button
-// function: create function that allows player to click "reset" button that returns the game board (cards and counters) to the starting position
-// event listener: create an event listener for each click on "reset"
-
-// create an event listener for each click, specifically on each button
